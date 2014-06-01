bb3a929af836e0dd681216a1f542ca5d2a78b3db
imranansari <imran.iansari@gmail.com>
2014-06-01 16:56:23 -0700

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,20 @@
+/**
+ * @jsx React.DOM
+ */
+
+'use strict';
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+React.renderComponent(
+  <CommentBox />,
+  document.getElementById('content')
+);
